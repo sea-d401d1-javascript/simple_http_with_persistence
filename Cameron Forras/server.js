3e6200c2fe9d29b6e77b3ede8634569835f3f8e2
Cameron Forras <cameronforras@hotmail.com>
2016-01-15 12:54:18 -0800

    Made adjustments to server and test. Works now.

--- Cameron Forras/server.js
+++ Cameron Forras/server.js
@@ -2,18 +2,23 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
-app.get('/data', function(req, res) {
-  var fileNames = fs.readdirSync(__dirname + '/data');
-  res.send(fileNames);
-  res.end();
-  console.log('Yo, reading directory!');
+app.get('/:filename', function(req, res) {
+  fs.readFile(__dirname + '/data/' + req.params.filename + '.json', function(err, data){
+    var myObj = JSON.parse(data.toString());
+    res.json(myObj);
+    console.log('Yo, reading directory!');
+  });
 });
 
-app.post('/data/:name', bodyParser.json(), function(req, res) {
-  fs.writeFileSync(__dirname + '/data' + req.params.num + '.json', req.body + ',');
-  res.send(req.body);
-  console.log('Yo, posting to file!');
+app.post('/:filename',jsonParser, function(req, res){
+  var postString = JSON.stringify(req.body);
+  fs.writeFile(__dirname + '/data/' + req.params.filename + '.json', postString, function(err){
+      if (err) throw err;
+      res.send(req.body);
+      console.log('Yo, posting to file!');
+    });
 });
 
 app.listen(3000, function() {
--- Cameron Forras/test/test.js
+++ Cameron Forras/test/test.js
@@ -3,31 +3,27 @@ var chaihttp = require('chai-http');
 chai.use(chaihttp);
 var expect = chai.expect;
 require(__dirname + '/../server');
-var fs = require('fs');
 
-describe('the server', function() {
-  it('should respond to a POST request', function(done) {
-    chai.request('localhost:3000')
-    .post('/data')
-    .send('{}')
-    .end(function(res, req) {
-      expect(req.text).to.eql('{}');
-      done();
-    });
+describe('a post request to /notes', function(){
+  it('should add to an existing json file', function(done){
+    chai.request('http://localhost:3000')
+      .post('/notes')
+      .send({note: 'Waaaaazzzaaaaap'})
+      .end(function(res){
+        expect(res.text).to.equal('{"note":"Waaaaazzzaaaaap"}');
+        expect(res).to.have.status(200);
+        done();
+      });
   });
 });
 
-describe('GET request', function() {
-  before(function() {
-    this.dirData = fs.readdirSync(__dirname + '/../data').toString();
-  });
-
-  it('should return array', function(done) {
+describe('a get request to /notes', function(){
+  it('should return a json object', function(done){
     chai.request('http://localhost:3000')
-      .get('/data')
+      .get('/notes')
       .end(function(err, res) {
-        expect(Array.isArray(res.body)).to.eql(true);
+        expect(res).to.have.status(200);
         done();
-      }.bind(this));
+      });
   });
 });

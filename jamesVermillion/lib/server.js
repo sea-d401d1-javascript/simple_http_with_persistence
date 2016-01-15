8c52576b228206022b4ffa298eb62fd53ccae43f
jamesvermillion <jamesvermillion@yahoo.com>
2016-01-14 23:24:50 -0800

    first draft done

---
+++ jamesVermillion/lib/server.js
@@ -0,0 +1,31 @@
+const Router = require(__dirname + '/router.js');
+const http = require('http');
+const fs = require('fs');
+
+var router = new Router();
+
+router.get( function(req, res) {
+  fs.readFile(__dirname + '/../json' + req.url, 'utf8',(err, data)=>{
+    if (err) return router.routes.FourOhFour(req, res);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(data);
+    res.end();  
+  });
+});
+
+router.post( function(req, res) {
+  //if (!req.url.endsWith('.json')) return router.routes.FourOhFour(req, res);
+  var requestJSON = '';
+  req.on('data', (data) => {
+    requestJSON += data;
+  });
+  req.on('end', () => {
+    fs.writeFile(__dirname + '/../json' + req.url, JSON.stringify(requestJSON) );
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write('your file has been stored as: ' + __dirname + '/../json' + req.url); 
+    res.end();
+  });
+});
+
+var server = http.createServer(router.route());
+server.listen(3000, () => console.log('server\'s up, dude! Port: 3000')); //eslint-disable-line
---
+++ jamesVermillion/test/test_persistance.js
@@ -0,0 +1,52 @@
+const server = require(__dirname + '/../lib/server.js'); //eslint-disable-line
+const chai = require('chai');
+chai.use(require('chai-http'));
+const request = chai.request;
+const expect = chai.expect;
+const fs = require('fs');
+
+describe('http server: GET', () => {
+  it('should show the buddy.json file', (done) => {
+    request('localhost:3000')
+      .get('/buddy.json')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.text).to.eql('{"buddy" : "you\'re my buddy"}');
+        done();
+      });
+  });
+  it('should show fuddy.json file couldn\'t be found', (done) => {
+    request('localhost:3000')
+      .get('/fuddy.json')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(404);
+        expect(res.text).to.eql('\"file not found\"');
+        done();
+      });
+  });
+});
+
+describe('http server: POST', function () {
+  var oldCount;
+  var newCount;
+  before(function(done) {
+    var list = fs.readdirSync(__dirname + '/../json');
+    oldCount = list.length;
+    request('localhost:3000')
+      .post('/fuddy.json')
+      .send({name: 'jim'})
+      .end((err, res) => { //eslint-disable-line
+        done();
+      });
+  });
+  after('clean up', function(){
+    fs.unlink(__dirname + '/../json/fuddy.json');
+  });
+  it('Should show we have a new file made', function () {
+    var newList = fs.readdirSync(__dirname + '/../json');
+    newCount = newList.length;
+    expect(newCount).to.eql(oldCount + 1); 
+  });
+});

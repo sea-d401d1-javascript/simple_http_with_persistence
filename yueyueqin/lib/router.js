7319e3c0df4bcb1bd86af6c7d32f3fd989f946bb
Yueyue07 <xww.qyy@gmail.com>
2016-01-14 23:24:11 -0800

    router construct function file

---
+++ yueyueqin/lib/router.js
@@ -0,0 +1,26 @@
+var Router = module.exports = exports = function() {
+  this.routes = {
+    'GET':{},
+    'POST':{},
+    '404': function(req,res) {
+      res.writeHead(200,'{Content-Type:"application/json"}');
+      res.write(JSON.stringify({msg: 'page not found'}));
+      return res.end();
+    }
+  };
+};
+
+Router.prototype.get = function(url,callback){
+  this.routes['GET'][url]=callback;
+};
+
+Router.prototype.post = function(url,callback){
+  this.routes['POST'][url]=callback;
+};
+
+Router.prototype.route = function(req,res) {
+  return (req,res) => {
+    var routerFunction = this.routes[req.method][req.url] || this.routes['404'];
+    routerFunction(req,res);
+  };
+};
---
+++ yueyueqin/server.js
@@ -0,0 +1,19 @@
+const Router = require(__dirname + '/lib/router');
+const http = require('http');
+const requestHandlers = require(__dirname + '/lib/requestHandlers');
+
+var router = new Router();
+
+router.get('/welcome', requestHandlers.get);
+
+router.post ('/note',requestHandlers.post);
+
+
+
+
+
+var server = http.createServer(router.route());
+
+server.listen(3000, () => {
+  console.log('server up');
+});

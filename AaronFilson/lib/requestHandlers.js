34429a604b1aac948a76243b5ad3bc43e241f156
Aaron Filson <aaronfilson@gmail.com>
2016-01-16 14:06:32 -0800

    Updated to use single file to store information and serve it back.

--- AaronFilson/lib/requestHandlers.js
+++ AaronFilson/lib/requestHandlers.js
@@ -1,4 +1,6 @@
 var url = require('url');
+const fs = require('fs');
+var storeFiles = require(__dirname + '/storeFiles');
 
 function start(request, response){
   console.log('in start handler');
@@ -11,10 +13,54 @@ function start(request, response){
 
 function store(request, response){
   console.log('in store handler');
-  if(request.method == 'PUT'){
-    console.log('handling PUT method in store');
+
+  function storeCallB(err, data){
+    if(err){
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.write("Error " + err + " on storing JSON.");
+      response.end();
+      return;
+    }
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Success on storing JSON");
+    response.end();
+    return;
   }
 
+  if(request.method == 'PUT'){
+    var dataObj;
+    console.log('handling PUT method in store');
+    request.on('data', function(chunk){
+      dataObj += chunk;
+
+    });
+    request.on('end', function(){
+      storeFiles.writeOne(dataObj, storeCallB);
+    });
+  }
+
+  if(request.method == 'POST'){
+    var dataObj;
+    console.log('handling PUT method in store');
+    request.on('data', function(chunk){
+      dataObj += chunk;
+
+    });
+    request.on('end', function(){
+      storeFiles.writeOne(dataObj, storeCallB);
+    });
+  }
+
+  if(request.method == 'GET'){
+    var fileP = __dirname + '/../dStore/basicFile.json';
+    storeFiles.readOne(fileP, (err, data) => {
+      response.writeHead(200, {"Content-Type": "application/json"});
+      response.write((err) ? err : data);
+      response.end();
+    });
+
+  }
 }
 
 exports.start = start;

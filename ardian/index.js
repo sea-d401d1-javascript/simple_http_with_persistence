fc1e1ddfc8eab1624c988e16a5405d55c0d36b83
Ardian Ajvazi <ardian_46@hotmail.com>
2016-01-16 16:28:48 -0800

    added package.json and router.js

--- ardian/index.js
+++ ardian/index.js
@@ -1,6 +1,1 @@
-module.exports = exports = require(__dirname + '/lib/router');
-
-
-// module.exports = exports = {
-//   Router: require(__dirname + '/lib/router')
-// }
+module.exports = exports = require(__dirname + '/lib/router.js');
